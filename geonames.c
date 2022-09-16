460cb21ae4d335103efc305fd9f530410c7ee236
Leonid Glazyrin <glazyrin.leonid@courrier.uqam.ca>
2022-09-15 22:31:19 -0400

    Print first line of cities

--- geonames.c
+++ geonames.c
@@ -1,9 +1,72 @@
+#define USAGE "\
+Usage: %s  <NUMBER OF CITIES>\n\
+\n\
+Display a table of the n most populous cities on stdout. The number of cities must not exceed 5000.\n\
+\n\
+If no argument is provided, the program prints this help and exit.\n\
+\n\
+Program parameters :\n\
+  NUMBER of cities         Number de cities to print (1..500)\n\
+  "
 #include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+
+#define FORMAT_TITRE    "%4s   %-20.20s   %-20.20s   %s\n"
+#define FORMAT_COLONNES "%4d   %-20.20s   %-20.20s   %10ld\n"
+
+/**
+ * ----------------
+ * Types de données
+ * ----------------
+ */
+struct Pays {
+    char nom[50]; // Le nom du pays
+    char code[3]; // Le code de deux lettres identifiant le pays
+};
+const struct Pays PAYS_BIDON = {"??", "??"};
+
+struct Ville {
+    char nom[100];    // Le nom ASCII de la ville
+    long population;  // La population
+    struct Pays pays; // Le pays dans laquelle la ville existe
+};
+const struct Ville VILLE_BIDON = {"??", -1, {"??", "??"}};
+
+/**
+ * -----------------------
+ * Codes d'erreur
+ * -----------------------
+ */
+
+enum error {
+    OK                      = 0,
+    ERREUR_NB_ARGS          = 1,
+    ERREUR_NB_VILLES        = 2,
+    ERREUR_ARGS_TYPE	    = 3
+};
+
 
 int main(int argc, char *argv[]) {
-    printf("argc = %d\n", argc);
-    for (unsigned int i = 0; i < argc; ++i) {
-        printf("argv[%d] = %s\n", i, argv[i]);
+    FILE *fptr;
+    char text[1000];
+
+    fptr = fopen("cities15000.txt", "r");
+    
+    if (fptr == NULL)
+    {
+        printf("Cannot open file \n");
+        return 1;
     }
+    
+    // while (fgets(text, 255, fptr)!=NULL) 
+    // {
+    //     printf ("%s", text);
+    // }
+
+    fgets(text, 1000, fptr);
+    printf ("%s", text);
+
+    fclose(fptr);
     return 0;
 }
